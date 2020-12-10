05bafb0202f75f42e6cab3c7b190a0102f9e1a14
Ilê Caian <ile.caian@gmail.com>
2020-12-10 12:31:39 -0300

    feat: constants for cross-usage
    Constants like `fontSize` and colors can be maintained in just
    one place as the source of truth. This helps in maintance and with
    the growth of the project it can be changed in all places just in
    one line.

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+export const MAIN_COLOR = '#296DD4';
+export const DANGER_COLOR = '#EC5858';
+export const DISABLED_COLOR = '#DEEBE1';
+
+export const TEXT_COLOR = '#1D2124';
+export const INVERTED_TEXT_COLOR = '#FFFFFF';
+
+export const BACKGROUND_APP_COLOR = '#E4F2FF';
+
+export const CARD_BACKGROUND_COLOR = '#C1D9FD';
---
+++ src/constants/fonts.js
@@ -0,0 +1,7 @@
+export const FONT_FAMILY = 'Helvetica';
+
+export const FONT_SIZE = 18;
+export const FONT_SIZE_SMALL = 14;
+
+export const TITLE_FONT_SIZE = 30;
+export const SUBTITLE_FONT_SIZE = 24;
